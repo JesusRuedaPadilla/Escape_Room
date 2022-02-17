4b8b7263236c0455bd5d4679fd34ccd7dc8381f5
Jesus Rueda Padilla <jruepad413@g.educaand.es>
2022-02-17 11:47:27 +0100

    cambio1

---
+++ src/Controller/IndividualController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndividualController extends AbstractController
+{
+    /**
+     * @Route("/sala/individual", name="individual")
+     */
+    public function index(): Response
+    {
+        return $this->render('individual/index.html.twig', [
+            'controller_name' => 'IndividualController',
+        ]);
+    }
+}
---
+++ src/Controller/ModernaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ModernaController extends AbstractController
+{
+    /**
+     * @Route("//moderna", name="moderna")
+     */
+    public function index(): Response
+    {
+        return $this->render('moderna/index.html.twig', [
+            'controller_name' => 'ModernaController',
+        ]);
+    }
+}
