92db64829b03dafabc2cc8732c4bf872ec09b681
Jesus Rueda Padilla <jruepad413@g.educaand.es>
2022-02-22 20:02:14 +0100

    Version restauracion

---
+++ src/Entity/Usuarios.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuariosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsuariosRepository::class)
+ */
+class Usuarios
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $dni;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apellidos;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $correo;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $fecha_nac;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contrasena;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDni(): ?string
+    {
+        return $this->dni;
+    }
+
+    public function setDni(string $dni): self
+    {
+        $this->dni = $dni;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(string $apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getCorreo(): ?string
+    {
+        return $this->correo;
+    }
+
+    public function setCorreo(string $correo): self
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    public function getFechaNac(): ?\DateTimeInterface
+    {
+        return $this->fecha_nac;
+    }
+
+    public function setFechaNac(\DateTimeInterface $fecha_nac): self
+    {
+        $this->fecha_nac = $fecha_nac;
+
+        return $this;
+    }
+
+    public function getContrasena(): ?string
+    {
+        return $this->contrasena;
+    }
+
+    public function setContrasena(string $contrasena): self
+    {
+        $this->contraseña = $contrasena;
+
+        return $this;
+    }
+}
